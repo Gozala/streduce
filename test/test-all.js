05fd514b870842ed09b82507946bf5b9d2f763f1
Irakli Gozalishvili <rfobic@gmail.com>
2012-11-01 22:25:36 -0700

    Update to reducers 0.5.x

--- test/test-all.js
+++
@@ -1,9 +0,0 @@
-/* vim:set ts=2 sw=2 sts=2 expandtab */
-/*jshint asi: true undef: true es5: true node: true browser: true devel: true
-         forin: true latedef: false globalstrict: true*/
-
-"use strict";
-
-exports["test reduce read stream"] = require("./read-stream")
-
-require("test").run(exports)
